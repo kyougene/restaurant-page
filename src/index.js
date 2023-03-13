c3423915c3885a8a18e845ea6110bcc6d42301ee
Kai Eugene <kai.yougene@gmail.com>
2023-03-13 09:45:44 -0700

    setting up the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hello')
+const page = document.getElementById('content');
+
+//buttons for toggling through the menu homepage and about 
+const homeButton = document.createElement('button');
+const aboutButton = document.createElement('button');
+const menuButton = document.createElement('button');
+
+//setting a class to the buttons for css styling
+homeButton.setAttribute('class', 'button');
+aboutButton.setAttribute('class', 'button');
+menuButton.setAttribute('class', 'button');
+
+//appending buttons to the content div
+page.appendChild(homeButton);
+page.appendChild(aboutButton);
+page.appendChild(menuButton);
+
+//div container for the pages 
+const pageContainer = document.createElement('div');
+pageContainer.setAttribute('class', 'container')
+page.appendChild(pageContainer);
+
+
